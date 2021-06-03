131cafb129cdeddc2a7b6b1ea73f46feb17434ff
Emily <emilye.seo@gmail.com>
2021-06-03 23:22:07 +0000

    Initiailizd git. Created templates

---
+++ script.js
@@ -0,0 +1,21 @@
+let player1Score = 0;
+let player2Score = 0;
+
+let buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+// selecting main 
+const main = document.querySelector("main");
+const endAlert = document.querySelector("#end-alert");
+const endDesc = document.querySelector("#end-desc");
+const returnMainBtn = document.querySelector("#")
+
+const scoreReset = document.querySelector(".reset-button");
+
+
+function printScore(player1Score) {
+    console.log(player1Score.score)
+}
+
+function printScore(player2Score) {
+    console.log(player2Score.score)
+}
